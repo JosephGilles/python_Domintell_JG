c1f44ec7cf6630251c89b33977e6b092755738d2
JosephGilles <67262096+JosephGilles@users.noreply.github.com>
2022-10-22 20:42:41 +0200

    Update var_status.py

--- domintell/messages/var_status.py
+++ domintell/messages/var_status.py
@@ -10,7 +10,6 @@ from domintell.messages import GenericDIStatusMessage
 VAR_COMMAND_CODE = "VAR"
 SYS_COMMAND_CODE = "SYS"
 
-
 class GenericVARStatusMessage(GenericDIStatusMessage):
     """
     Generic VAR module status
@@ -28,9 +27,9 @@ class GenericVARStatusMessage(GenericDIStatusMessage):
         # we can not identify variable by serialNumber,
         # so we prepend moduleType/command_code in the front
         if dataType == "O":
-            self.populateA(self.moduleType + serialNumber, dataType, dataString)
-        elif dataType == "D":
             self.populateD(self.moduleType + serialNumber, dataType, dataString)
+        elif dataType == "D":
+            self.populateA(self.moduleType + serialNumber, dataType, dataString)
 
     def populateD(self, serialNumber, dataType, dataString):
         assert isinstance(dataString, str)
@@ -56,7 +55,6 @@ class GenericVARStatusMessage(GenericDIStatusMessage):
         json_dict['var'] = self.var
         return json.dumps(json_dict)
 
-
 class VARStatusMessage(GenericVARStatusMessage):
     """
     VAR  status
@@ -64,15 +62,12 @@ class VARStatusMessage(GenericVARStatusMessage):
     def __init__(self, address=None):
         GenericVARStatusMessage.__init__(self, VAR_COMMAND_CODE)
 
-
 class SYSStatusMessage(GenericVARStatusMessage):
     """
     SYS  status
     """
     def __init__(self, address=None):
         GenericVARStatusMessage.__init__(self, SYS_COMMAND_CODE)
-
-
 
 domintell.register_command(VAR_COMMAND_CODE, VARStatusMessage)
 domintell.register_command(SYS_COMMAND_CODE, SYSStatusMessage)
